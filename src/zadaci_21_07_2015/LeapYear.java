42d96320f65b847fb6239b5a7cd7ff237087100c
BILD-IT <BILD-IT@BILD-IT-PC2>
2015-07-22 01:43:24 +0200

    done

--- src/zadaci_21_07_2015/LeapYear.java
+++ src/zadaci_21_07_2015/LeapYear.java
@@ -11,11 +11,9 @@ public class LeapYear {
 
 	public static void main(String[] args) {
 
-		int countLeapYears = 0; // counts how many leap years there are from 101
-								// to 2100.
+		int countLeapYears = 0; // counts how many leap years there are from 101 to 2100.
 
-		for (int year = 101; year <= 2100; year++) { // we start from 101 year
-														// to 2100 year
+		for (int year = 101; year <= 2100; year++) { // we start from 101 year to 2100 year
 
 			// a year is leap if its divisible by 4 but not by 100 or if its
 			// divisible by 400
@@ -25,8 +23,7 @@ public class LeapYear {
 			if (isLeapYear) { // if its true, then print it
 				System.out.print(year + " ");
 				countLeapYears++; // increase for one the number of leap years
-				if (countLeapYears % 10 == 0) { // if we have a number of leap
-												// years that
+				if (countLeapYears % 10 == 0) { // if we have a number of leap years that
 					// is divisble by 10 then go to the next line
 					System.out.println();
 				}
