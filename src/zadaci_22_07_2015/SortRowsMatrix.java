c47ee8184a151fd618fdb5ebf81babff26ef37b9
BILD-IT <BILD-IT@BILD-IT-PC2>
2015-07-23 00:57:16 +0200

    5.exercise

---
+++ src/zadaci_22_07_2015/SortRowsMatrix.java
@@ -0,0 +1,45 @@
+package zadaci_22_07_2015;
+
+import java.util.Arrays;
+
+//Implementirati sljedeæu metodu da sortira redove u 2D nizu.
+//public static double[ ][ ] sortRows(double[ ][ ] array)
+//Napisati testni program koji pita korisnika da unese 3x3 matricu 
+//(ili da pita korisnika koliku matricu želi unijeti) te mu ispisuje 
+//na konzoli matricu sa sortiranim redovima - od najmanjeg broja do najveæeg.
+
+/**
+ * @author Marina Sljivic
+ *
+ */
+public class SortRowsMatrix {
+
+	/**
+	 * @param 2D array (a matrix) type double
+	 * @return the array with sorted rows only
+	 */
+	public static double[][] sortRows(double[][] array){
+		for(int i=0;i<array.length;i++){
+			Arrays.sort(array[i]);
+		}
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		//usa a method from another class that makes a 2Darray (a matrix) of user's inputs
+		double[][] matrix=Double2DArrayStuff.input2DArray(); //take the 2D array from the user
+		
+		//sort the rows of the matrix
+		matrix=sortRows(matrix);
+		
+		//print the matrix
+		for(int i=0;i<matrix.length;i++){
+			for(int j=0;j<matrix[0].length;j++){
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
